af505d74a6cad14f0ee5bffb05c6cd1b023a4bb7
bamblebam <burhanizrangwala@gmail.com>
2023-10-20 01:46:18 +0530

    bugfix: logging and api fix

--- src/router/health/api.health.ts
+++ src/router/health/api.health.ts
@@ -1,13 +1,14 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import HealthController from "./controller.health";
 import type { appHealthCheckResponse } from "./helper.health";
 
 const router: Router = Router();
 
-router.get("/", async () => {
+router.get("/", async (req: Request, res: Response) => {
+  console.log(`${req.originalUrl}-[${req.method}]`);
   const result: appHealthCheckResponse =
     await HealthController.appHealthCheck();
-  return result;
+  res.status(result.status).json(result);
 });
 
 export default router;

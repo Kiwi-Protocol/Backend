1a458bbd35da55c3231f266fb7ff64b4d11bfbe5
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-10-20 12:52:57 +0530

    ft: mint utils working

---
+++ src/utils/mint.ts
@@ -0,0 +1,38 @@
+import {
+  CURRENT_CHAIN,
+  MINTER_ABI,
+  MINTER_ADDRESS,
+  PRIVATE_KEY,
+} from "../constants";
+import { Address, createWalletClient, http } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+
+export async function mint(to: Address, tokenURI: string) {
+  if (!PRIVATE_KEY) {
+    throw new Error("No Private Key found. Check your environment");
+  }
+
+  try {
+    const account = privateKeyToAccount(PRIVATE_KEY as any);
+
+    const walletClient = createWalletClient({
+      account,
+      chain: CURRENT_CHAIN,
+      transport: http(),
+    });
+
+    const result = await walletClient.writeContract({
+      abi: MINTER_ABI,
+      address: MINTER_ADDRESS,
+      functionName: "mint",
+      args: [to, tokenURI],
+    });
+
+    console.log("Result of Mint", result);
+
+    return result;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}

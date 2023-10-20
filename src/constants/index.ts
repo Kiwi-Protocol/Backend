698268c6ded84e58f766b242dd7b284596f173b1
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-10-20 21:48:34 +0530

    chore: switch to polygon mainnet

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,13 +1,13 @@
-import { avalancheFuji } from "viem/chains";
+import { polygon } from "viem/chains";
 import Minter from "./Minter.json";
 import { Address } from "viem";
 
 require("dotenv/config");
 
-export const WALLET_ADDRESS = "0xe00019DE7B0fBC7B8C7EAD6c0bB470Ec1bA4bA24" // If Required
+export const WALLET_ADDRESS = "0xe00019DE7B0fBC7B8C7EAD6c0bB470Ec1bA4bA24"; // If Required
 export const MINTER_ADDRESS = Minter.address as Address;
 export const MINTER_ABI = Minter.abi;
-export const CURRENT_CHAIN = avalancheFuji
+export const CURRENT_CHAIN = polygon;
 
 export const PRIVATE_KEY = process.env.PRIVATE_KEY;
-export const SPHERON_TOKEN = process.env.SPHERON_TOKEN
+export const SPHERON_TOKEN = process.env.SPHERON_TOKEN;
